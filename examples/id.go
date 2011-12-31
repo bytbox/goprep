ae671f55f0d90d05c95b4ba6e06b06de63f42869
Scott Lawrence <bytbox@gmail.com>
2011-12-31 12:42:21 -0500

    Using StdInit in example

--- examples/id.go
+++ examples/id.go
@@ -2,12 +2,10 @@ package main
 
 import (
 	"github.com/bytbox/goprep"
-	"os"
 )
 
 func main() {
-	tokIn := goprep.Read(os.Stdin)
-	tokOut, done := goprep.Write(os.Stdout)
+	tokIn, tokOut, done := goprep.StdInit()
 	for tok := range tokIn {
 		str := tok.Str
 		tokOut <- str
