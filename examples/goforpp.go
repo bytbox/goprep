99f42ec6649da2817e1445447b7791ad57bec0e5
Scott Lawrence <bytbox@gmail.com>
2012-01-01 11:39:30 -0500

    goforpp now works

--- examples/goforpp.go
+++ examples/goforpp.go
@@ -2,25 +2,65 @@ package main
 
 import (
 	. "github.com/bytbox/goprep/goprep"
+	"go/token"
 	"os"
 )
+
+type BoolStack struct {
+	Val  bool
+	Base *BoolStack
+}
+
+func Push(s *BoolStack, i bool) *BoolStack {
+	return &BoolStack{i, s}
+}
+
+func Peek(s *BoolStack) bool {
+	return s.Val
+}
+
+func Pop(s *BoolStack) (*BoolStack, bool) {
+	return s.Base, s.Val
+}
 
 func main() {
 	p := PipeInit(os.Stdin)
 	Lines(p)
 
 	nIn := make(chan TokenInfo)
-	// we pass everything through until we reach 'gofor'
+	s := (*BoolStack)(nil)
 	go func(in chan TokenInfo,
 		tOut chan TokenInfo,
 		out chan string,
 		sync chan interface{}) {
 		for tok := range in {
+			if tok.Token == token.LBRACE {
+				s = Push(s, false)
+			}
+			if tok.Token == token.RBRACE {
+				var b bool
+				s, b = Pop(s)
+				if b {
+					out <- "}}()"
+					sync <- nil
+					continue
+				}
+			}
 			if tok.Str != "gofor" {
 				out <- tok.Str
 				sync <- nil
 				continue
 			}
+			out <- "go func() { for"
+			sync <- nil
+			tok = <-in
+			for tok.Token != token.LBRACE {
+				out <- tok.Str
+				sync <- nil
+				tok = <-in
+			}
+			out <- " {"
+			s = Push(s, true)
 			sync <- nil
 		}
 		close(tOut)
