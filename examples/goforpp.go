3a1eacaa6b7649aec49f5be0d626320699e1616d
Scott Lawrence <bytbox@gmail.com>
2012-01-01 10:08:07 -0500

    goforpp now drops all 'gofor's

--- examples/goforpp.go
+++ examples/goforpp.go
@@ -8,6 +8,24 @@ import (
 func main() {
 	p := PipeInit(os.Stdin)
 	Lines(p)
+
+	nIn := make(chan TokenInfo)
+	// we pass everything through until we reach 'gofor'
+	go func(in chan TokenInfo,
+		tOut chan TokenInfo,
+		out chan string,
+		sync chan interface{}) {
+		for tok := range in {
+			if tok.Str != "gofor" {
+				out <- tok.Str
+				continue
+			}
+			sync <- nil
+		}
+		close(tOut)
+	}(p.Input, nIn, p.Output, p.Sync)
+	p.Input = nIn
+
 	Discard(p)
 	PipeEnd(p, os.Stdout)
 }
